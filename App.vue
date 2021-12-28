5ac0f32c5e5bd2cde1277c137f00113680dc0cf6
xuchangfan <xuchangfan@sina.cn>
2021-12-28 21:07:00 +0800

    app也修改为通过云服务走

--- App.vue
+++ App.vue
@@ -11,86 +11,82 @@
 			// #ifdef APP-PLUS  
 			const that = this
 			plus.runtime.getProperty(plus.runtime.appid, function() { 
-					const wdszSaleChannel = plus.runtime.channel 
-			    uni.request({  
-			        url: 'http://wdsz2.huojiangame.com:11658/ljxz/update.php',
-			        success: (result) => {  
-			            var data = result.data;
-									var wgtUrl = data.wgtUrl
-									var pkgUrl = ''
-									if (wdszSaleChannel === 'test') {
-										pkgUrl = data.pkgUrl
-									} else if (wdszSaleChannel === '1') {
-										pkgUrl = data.pkgUrl1
-									}  else if (wdszSaleChannel === '2') {
-										pkgUrl = data.pkgUrl2
-									} else if (wdszSaleChannel === '3') {
-										pkgUrl = data.pkgUrl3
-									} else if (wdszSaleChannel === '4') {
-										pkgUrl = data.pkgUrl4
-									} else if (wdszSaleChannel === '5') {
-										pkgUrl = data.pkgUrl5
-									} else if (wdszSaleChannel === '7') {
-										pkgUrl = data.pkgUrl7
-									} else if (wdszSaleChannel === '8') {
-										pkgUrl = data.pkgUrl8
-									} else if (wdszSaleChannel === '31') {
-										pkgUrl = data.pkgUrl31
-									}
-			            if (data.version > that.$global.wdszVersion && wgtUrl && data.updateType ===1 ) {  // 热更新
-											// uni.showModal({
-											//     title: "发现新版本",
-											//     content: "确认下载更新",
-											//     success: (res) => {
-											//         if (res.confirm) {//当用户确定更新，执行更新
-											//             that.doUpData();
-											//         } else if (res.cancel) {
-											//             // console.log('用户点击取消');
-											//         }
-											//     }
-											// })
-			                uni.downloadFile({  
-			                    url: wgtUrl,  
-			                    success: (downloadResult) => {  
-			                        if (downloadResult.statusCode === 200) {  
-			                            plus.runtime.install(downloadResult.tempFilePath, {  
-			                                force: true  
-			                            }, function() {  
-			                                console.log('install success...');  
-																			uni.showToast({
-																				title:"辅助更新成功，即将重启",
-																				duration:1000,
-																				icon:'none'
-																			})
-																			setTimeout(function() {
-																			  plus.runtime.restart();
-																			}, 1100)
-			                            }, function(e) {  
-			                                console.error('install fail...'); 
-																			 uni.showToast({
-																			 	title:"辅助更新失败",
-																				duration:2000,
-																				icon:'none'
-																			 })
-			                            });  
-			                        }
-			                    }  
-			                }); 
-									}
-									if (data.version > that.$global.wdszVersion && pkgUrl && data.updateType === 2 ) { //整包更新
-										uni.showModal({ //提醒用户更新  
-											title: "更新提示",  
-											content: data.note,  
-											success: (res) => {  
-												if (res.confirm) {  
-													plus.runtime.openURL(pkgUrl);  
-												}  
-											}  
-										})  
-									}
-									  
-			        }  
-			    });  
+					const wdszSaleChannel = plus.runtime.channel
+					getUpdate().then(result => {
+						var data = result;
+						var wgtUrl = data.wgtUrl
+						var pkgUrl = ''
+						if (wdszSaleChannel === 'test') {
+							pkgUrl = data.pkgUrl
+						} else if (wdszSaleChannel === '1') {
+							pkgUrl = data.pkgUrl1
+						}  else if (wdszSaleChannel === '2') {
+							pkgUrl = data.pkgUrl2
+						} else if (wdszSaleChannel === '3') {
+							pkgUrl = data.pkgUrl3
+						} else if (wdszSaleChannel === '4') {
+							pkgUrl = data.pkgUrl4
+						} else if (wdszSaleChannel === '5') {
+							pkgUrl = data.pkgUrl5
+						} else if (wdszSaleChannel === '7') {
+							pkgUrl = data.pkgUrl7
+						} else if (wdszSaleChannel === '8') {
+							pkgUrl = data.pkgUrl8
+						} else if (wdszSaleChannel === '31') {
+							pkgUrl = data.pkgUrl31
+						}
+						if (data.version > that.$global.wdszVersion && wgtUrl && data.updateType ===1 ) {  // 热更新
+								// uni.showModal({
+								//     title: "发现新版本",
+								//     content: "确认下载更新",
+								//     success: (res) => {
+								//         if (res.confirm) {//当用户确定更新，执行更新
+								//             that.doUpData();
+								//         } else if (res.cancel) {
+								//             // console.log('用户点击取消');
+								//         }
+								//     }
+								// })
+						    uni.downloadFile({  
+						        url: wgtUrl,  
+						        success: (downloadResult) => {  
+						            if (downloadResult.statusCode === 200) {  
+						                plus.runtime.install(downloadResult.tempFilePath, {  
+						                    force: true  
+						                }, function() {  
+						                    console.log('install success...');  
+																uni.showToast({
+																	title:"辅助更新成功，即将重启",
+																	duration:1000,
+																	icon:'none'
+																})
+																setTimeout(function() {
+																  plus.runtime.restart();
+																}, 1100)
+						                }, function(e) {  
+						                    console.error('install fail...'); 
+																 uni.showToast({
+																 	title:"辅助更新失败",
+																	duration:2000,
+																	icon:'none'
+																 })
+						                });  
+						            }
+						        }  
+						    }); 
+						}
+						if (data.version > that.$global.wdszVersion && pkgUrl && data.updateType === 2 ) { //整包更新
+							uni.showModal({ //提醒用户更新  
+								title: "更新提示",  
+								content: data.note,  
+								success: (res) => {  
+									if (res.confirm) {  
+										plus.runtime.openURL(pkgUrl);  
+									}  
+								}  
+							})  
+						}
+					})
 			});  
 			// #endif
 		},
